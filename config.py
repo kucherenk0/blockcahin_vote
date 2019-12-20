a755e0e99a19ee6b06bd5592b036ef2f8ba63c5c
kucher <kucherenko.av@phystech.edu>
2019-12-21 01:17:07 +0300

    Deleted default transaction

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 from pygost.gost3410 import CURVES
-from transaction import Transaction
 
 ELLIPTIC_CURVE = CURVES["id-tc26-gost-3410-12-512-paramSetA"]
 TARGET = 0x00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 DEFAULT_PROOF = 100
-DEFAULT_TRANSACTION = Transaction('genesys', 'genesys', 0)
 FAIL = -1
 SUCCESS = 0
 
-candidates_list = ['Владимир',
+CANDIDATES_LIST = ['Владимир',
                    'Путин',
                    'Молодец',
                    'Политик',
